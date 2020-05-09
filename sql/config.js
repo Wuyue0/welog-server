502525d89c1cde670e0f8afb411a4ef5e0c2c25b
wuyue <wuyue12@lenovo.com>
2020-05-09 12:24:44 +0800

    111

--- sql/config.js
+++ sql/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host        :   'localhost', //数据库所在的服务器的域名或者ip地址
     user        :   'root',  //登录数据库的账号
-    password    :   'wuyue521', //登录数据库的密码
+    password    :   '123456', //登录数据库的密码
     database    :   'admin_database' //数据库名称
 }
   
